00953a1a958e2d551dd0ed0577a575ca70b803c1
caroibanez <caroibanez@iMac-de-Carolina.local>
2022-09-01 20:40:55 -0300

    Entregable FrontEndIII

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+import {useState} from "react";
+
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+function App() {
+  const [itemCarrito, setItemCarrito] = useState(0);
+  function agregarItem(){
+    setItemCarrito(itemCarrito + 1);
+  }
+  return (
+    <div className="App">
+      <Cabecera stockTotal={itemCarrito}/>
+      <Listado onComprarItem={agregarItem}/>
+    </div>
+  );
+}
+
+export default App;
